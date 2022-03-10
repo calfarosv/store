85e318b715ac9591ec9bb717372b6efaaf0aff22
calfarosv <calfaro@cel.gob.sv>
2022-03-10 09:55:39 -0600

    10.03.2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CodeComponent } from './pages/code/code.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "code", component: CodeComponent, },
+  {path: "**", redirectTo: '', pathMatch: 'full', }, //Cuando una ruta no exista, lo envía a home
+]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store';
+  title = 'Proyecto de Pruebas';
+
+  getName(): void {
+    console.log('Hola')
+  }
+
 }
